d35bc9b56950f3ee41a4c71dfbcb119a0ddcd0ef
Valdenise Costa <valdenisecosta@hotmail.com>
2023-10-12 01:30:55 -0400

    foi criado o objeto UserModel com usuário, e definido os atributos nome, nome_usuario e senhanameuser_name e password, todas no tipo String.

--- src/main/java/br/com/valgroup/todolist/user/UserController.java
+++ src/main/java/br/com/valgroup/todolist/user/UserController.java
@@ -1,5 +1,34 @@
 package br.com.valgroup.todolist.user;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificador
+ * public
+ * private
+ * protected
+ */
+ @RestController
+ @RequestMapping("/users")
 public class UserController {
-    
+
+    /**
+     * String (texto)
+     * Integer (int)
+     * Double (double) 0.000
+     * Float (float) 0.000
+     * char (A C)
+     * Date (data)
+     * void ()
+     */
+    /**
+     * Body da requisição
+     */
+    @PostMapping("/")
+    public void create(@RequestBody UserModel UserModel) {
+        System.out.println(UserModel.name);
+    }
 }
